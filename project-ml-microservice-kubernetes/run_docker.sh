8a143413d665175f2e2d6b0b7e00a334f506fae6
Florian <foobar@localhost.com>
2020-02-16 10:54:57 -0800

    feat: completed run_docker.sh

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --network=host -t udacity-house-price-model .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run --rm -p 80:80 --name udacity-house-price-model udacity-house-price-model
