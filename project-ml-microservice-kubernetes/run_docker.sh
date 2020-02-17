7b23f5287e88b6c8b7a12b50e7475409b186dda9
Florian <foobar@localhost.com>
2020-02-17 12:52:58 -0800

    fix: minor changes

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --network=host -t udacity-house-price-model .
+docker build --network=host -t house-price-model .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3: 
 # Run flask app
-docker run --rm -p 80:80 --name udacity-house-price-model udacity-house-price-model
+docker run --rm -p 80:80 --name house-price-model house-price-model
